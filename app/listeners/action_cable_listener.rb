593f099aa79037ad11b34c1da302cef60e54bd27
Inga Rondel <90716651+rondelinga@users.noreply.github.com>
2026-05-15 14:36:44 +0300

    cwt-83/fix: displaying the source

--- app/listeners/action_cable_listener.rb
+++ app/listeners/action_cable_listener.rb
@@ -175,6 +175,14 @@ class ActionCableListener < BaseListener
     broadcast(account, [user.pubsub_token], CONVERSATION_MENTIONED, conversation.push_event_data)
   end
 
+  def agent_updated(event)
+    user = event.data[:user]
+    account = Account.find_by(id: event.data[:account_id])
+    return if user.blank? || account.blank?
+  
+    broadcast(account, [user.pubsub_token], AGENT_UPDATED, { id: user.id })
+  end
+
   private
 
   def account_token(account)
