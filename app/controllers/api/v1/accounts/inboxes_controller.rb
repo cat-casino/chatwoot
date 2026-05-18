6af5b6d39d0907a4a985638428821f1dc2a7fd4b
Inga Rondel <90716651+rondelinga@users.noreply.github.com>
2026-05-18 13:58:34 +0300

    cwt-74/feat: add public name for inbox

--- app/controllers/api/v1/accounts/inboxes_controller.rb
+++ app/controllers/api/v1/accounts/inboxes_controller.rb
@@ -175,7 +175,7 @@ class Api::V1::Accounts::InboxesController < Api::V1::Accounts::BaseController
   end
 
   def inbox_attributes
-    [:name, :avatar, :greeting_enabled, :greeting_message, :enable_email_collect, :csat_survey_enabled,
+    [:name, :public_name, :avatar, :greeting_enabled, :greeting_message, :enable_email_collect, :csat_survey_enabled,
      :enable_auto_assignment, :working_hours_enabled, :out_of_office_message, :timezone, :allow_messages_after_resolved,
      :lock_to_single_conversation, :portal_id, :sender_name_type, :priority_group_id, :business_name,
      { csat_config: [:display_type, :message, :button_text, :language,
